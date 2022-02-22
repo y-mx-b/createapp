c74fc4af55eb6e869e733227b5f6a0ff1437ffce
Slope <matthewna1031@gmail.com>
2022-02-22 20:21:54 +0900

    Added Path.swift as a dependency

--- Package.swift
+++ Package.swift
@@ -13,12 +13,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        .package(url: "https://github.com/mxcl/Path.swift.git", from: "1.4.0"),
     ],
     targets: [
         .executableTarget(
             name: "createapp",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Path", package: "Path.swift"),
             ]
         ),
         .testTarget(
