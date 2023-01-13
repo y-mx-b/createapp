6a959eaec0c08ce84f5465fe6fbe076b63dc1385
Slope <linearfunc@proton.me>
2023-01-13 22:33:31 +0900

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "createapp",
+    products: [
+        .executable(name: "createapp", targets: ["createapp"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
     ],
